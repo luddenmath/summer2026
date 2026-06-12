0e176d1c0bd0fa4e55c88fedc4ee6109a969589d
luddenmath <luddenjacob2@gmail.com>
2026-06-12 14:35:17 -0500

    Update script_cjf.js

--- script_cjf.js
+++ script_cjf.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
       totalCorrect++; // Increment total correct count
       timeoutValue = 400;
     } else {
-      resultElement.innerHTML = `Incorrect. \n The correct answer is ${answer}. \n Try again!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`;
+      resultElement.innerHTML = `Incorrect. \n The correct answer is ${answer}. \n\n\n\n\n\n\\n Try again!??????????????????`;
       resultElement.style.color = 'red';
       currentStreak = 0; // Reset the streak on incorrect answer
       totalIncorrect++; // Increment total incorrect count
